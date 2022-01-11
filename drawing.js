e3c0027a934a31d9fbdcda7208fc5b19cc705d1c
Matteo Badenchini <37813897+bade27@users.noreply.github.com>
2022-01-11 17:06:18 +0100

    motion of  the bar

--- drawing.js
+++ drawing.js
@@ -5,28 +5,60 @@ var shaderDir;
 var modelsDir;
 var models = new Array();
 
+var dirLightAlpha;
+var dirLightBeta;
+var directionalLight;
+var directionalLightColor;
+
+var cubeMaterialColor;
+var lastUpdateTime;
+
+var modelsNormalMatrix = new Array(), modelsWorldMatrix = new Array();
+var positionAttributeLocation = new Array(), normalAttributeLocation = new Array();  
+var matrixLocation = new Array(), materialDiffColorHandle = new Array();
+var lightDirectionHandle = new Array(), lightColorHandle = new Array();
+var normalMatrixPositionHandle = new Array(), vertexMatrixPositionHandle = new Array();
+var materialDiffColorHandle = new Array();
+var vaos = new Array();
+  
+var xBar;
+var yBar;
+var zBar;
+
+var limitLeft = -4.5;
+var limitRight = 4.5;
+
+var perspectiveMatrix;
+var viewMatrix;
+var modelVertices;
+var modelNormals;
+var modelIndices;
+
 function main() {
 
   //directional light
-  var dirLightAlpha = utils.degToRad(180);
-  var dirLightBeta  = utils.degToRad(100);
-  var directionalLight = [Math.cos(dirLightAlpha) * Math.cos(dirLightBeta),
+  dirLightAlpha = utils.degToRad(180);
+  dirLightBeta  = utils.degToRad(100);
+  directionalLight = [Math.cos(dirLightAlpha) * Math.cos(dirLightBeta),
               Math.sin(dirLightAlpha), Math.cos(dirLightAlpha) * Math.sin(dirLightBeta)];
-  var directionalLightColor = [0.1, 1.0, 1.0];
+  directionalLightColor = [0.1, 1.0, 1.0];
   //material color
-  var cubeMaterialColor = [0.5, 0.5, 0.5];
-  var lastUpdateTime = (new Date).getTime();
+  cubeMaterialColor = [0.5, 0.5, 0.5];
+  lastUpdateTime = (new Date).getTime();
 
-  var modelsNormalMatrix = new Array(), modelsWorldMatrix = new Array();
-  var positionAttributeLocation = new Array(), normalAttributeLocation = new Array();  
-  var matrixLocation = new Array(), materialDiffColorHandle = new Array();
-  var lightDirectionHandle = new Array(), lightColorHandle = new Array();
-  var normalMatrixPositionHandle = new Array(), vertexMatrixPositionHandle = new Array();
-  var materialDiffColorHandle = new Array();
-  var vaos = new Array();
-
-  modelsWorldMatrix[0] = utils.MakeWorld(0.5, 0.0, -4.0, 0.0, 0.0, 0.0, 1.0);
-  modelsWorldMatrix[1] = utils.MakeWorld( 0.0, -1.0, -14.0, 0.0, 0.0, 0.0, 1.0);
+  modelsNormalMatrix = new Array(), modelsWorldMatrix = new Array();
+  positionAttributeLocation = new Array(), normalAttributeLocation = new Array();  
+  matrixLocation = new Array(), materialDiffColorHandle = new Array();
+  lightDirectionHandle = new Array(), lightColorHandle = new Array();
+  normalMatrixPositionHandle = new Array(), vertexMatrixPositionHandle = new Array();
+  materialDiffColorHandle = new Array();
+  vaos = new Array();
+  
+  xBar = 0.5;
+  yBar = 0.0;
+  zBar = -4.5;
+  modelsWorldMatrix[0] = utils.MakeWorld(xBar, yBar, zBar, 0.0, 0.0, 0.0, 1.0);
+  modelsWorldMatrix[1] = utils.MakeWorld( 0.0, -1.0, zBar-10.2, 0.0, 0.0, 0.0, 1.0);
 
   modelsNormalMatrix[0] = utils.invertMatrix(utils.transposeMatrix(modelsWorldMatrix[0]));
 
@@ -53,13 +85,16 @@ function main() {
   positionAttributeLocation[2] = gl.getAttribLocation(programs[2], "inPosition");  
   matrixLocation[2] = gl.getUniformLocation(programs[2], "matrix");
   
-  var perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width/gl.canvas.height, 0.1, 100.0);
-  var viewMatrix = utils.MakeView(0.0, 2.0, 0.0, -5.0, 0.0);
+  // persepctive and view
+  perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width/gl.canvas.height, 0.1, 100.0);
+  viewMatrix = utils.MakeView(0.0, 2.0, 0.0, -5.0, 0.0);
   
   //------------------------------------------------------------------------------------------------
-  var modelVertices = new Array();
-  var modelNormals = new Array();
-  var modelIndices = new Array();
+  // OBJECTS DEFINITION
+  //------------------------------------------------------------------------------------------------
+  modelVertices = new Array();
+  modelNormals = new Array();
+  modelIndices = new Array();
   
   // bar
   modelVertices[0] = models[0].vertices;
@@ -105,51 +140,49 @@ function main() {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(modelIndices[1]), gl.STATIC_DRAW);
   //---------------------------------------------------------------------------------------------
-
   drawScene();
+}
 
-  function animate(){
-                  
-  }
+function animate(){
+}
 
-  function drawScene() {
-    animate();
+function drawScene() {
+  animate();
 
-    gl.clearColor(0.85, 0.85, 0.85, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-
-    // bar
-    gl.useProgram(programs[0]);
-    var viewWorldMatrix = utils.multiplyMatrices(viewMatrix, modelsWorldMatrix[0]);
-    var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
-    gl.uniformMatrix4fv(matrixLocation[0], gl.FALSE, utils.transposeMatrix(projectionMatrix));
-
-    gl.uniformMatrix4fv(vertexMatrixPositionHandle[0], gl.FALSE, utils.transposeMatrix(modelsWorldMatrix[0]));
-    gl.uniformMatrix4fv(normalMatrixPositionHandle[0], gl.FALSE, utils.transposeMatrix(modelsNormalMatrix[0]));
-
-    gl.uniform3fv(materialDiffColorHandle[0], cubeMaterialColor);
-    gl.uniform3fv(lightColorHandle[0],  directionalLightColor);
-    gl.uniform3fv(lightDirectionHandle[0],  directionalLight);
-
-    gl.bindVertexArray(vaos[0]);
-    gl.drawElements(gl.TRIANGLES, modelIndices[0].length, gl.UNSIGNED_SHORT, 0 );
+  gl.clearColor(0.85, 0.85, 0.85, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-    // base
-    gl.useProgram(programs[2]);
-    viewWorldMatrix = utils.multiplyMatrices(viewMatrix, modelsWorldMatrix[1]);
-    projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
-    gl.uniformMatrix4fv(matrixLocation[2], gl.FALSE, utils.transposeMatrix(projectionMatrix));
+  // bar
+  gl.useProgram(programs[0]);
+  modelsWorldMatrix[0] = utils.MakeWorld(xBar, yBar, zBar, 0.0, 0.0, 0.0, 1.0);
+  var viewWorldMatrix = utils.multiplyMatrices(viewMatrix, modelsWorldMatrix[0]);
+  var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
+  gl.uniformMatrix4fv(matrixLocation[0], gl.FALSE, utils.transposeMatrix(projectionMatrix));
 
-    gl.bindVertexArray(vaos[1]);
-    gl.drawElements(gl.TRIANGLES, modelIndices[1].length, gl.UNSIGNED_SHORT, 0 );
-    
+  gl.uniformMatrix4fv(vertexMatrixPositionHandle[0], gl.FALSE, utils.transposeMatrix(modelsWorldMatrix[0]));
+  gl.uniformMatrix4fv(normalMatrixPositionHandle[0], gl.FALSE, utils.transposeMatrix(modelsNormalMatrix[0]));
 
-    // drawScene
-    window.requestAnimationFrame(drawScene);
-  }
+  gl.uniform3fv(materialDiffColorHandle[0], cubeMaterialColor);
+  gl.uniform3fv(lightColorHandle[0],  directionalLightColor);
+  gl.uniform3fv(lightDirectionHandle[0],  directionalLight);
 
+  gl.bindVertexArray(vaos[0]);
+  gl.drawElements(gl.TRIANGLES, modelIndices[0].length, gl.UNSIGNED_SHORT, 0 );
+
+
+  // base
+  gl.useProgram(programs[2]);
+  viewWorldMatrix = utils.multiplyMatrices(viewMatrix, modelsWorldMatrix[1]);
+  projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
+  gl.uniformMatrix4fv(matrixLocation[2], gl.FALSE, utils.transposeMatrix(projectionMatrix));
+
+  gl.bindVertexArray(vaos[1]);
+  gl.drawElements(gl.TRIANGLES, modelIndices[1].length, gl.UNSIGNED_SHORT, 0 );
+  
+
+  // drawScene
+  //window.requestAnimationFrame(drawScene);
 }
 
 async function init(){
@@ -199,5 +232,20 @@ async function init(){
   main();
 }
 
-window.onload = init;
+window.addEventListener("keydown", function keyFunction(e){
+ 
+  if (e.key == "ArrowLeft") {
+    if (xBar > limitLeft) {
+      xBar-=0.5;
+    }
+  }
+  if (e.key == "ArrowRight") {  // Right arrow
+    if (xBar < limitRight) {
+      xBar+=0.5;
+    }
+  } 
 
+  //If you put it here instead, you will redraw the cube only when the camera has been moved
+  window.requestAnimationFrame(drawScene);
+});
+window.onload = init;
