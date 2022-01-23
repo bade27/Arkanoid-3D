f0e7be0a4d094d7da0d4390ce6885dbdc3f45edf
Matteo Badenchini <37813897+bade27@users.noreply.github.com>
2022-01-23 19:28:21 +0100

    the game kinda works

--- drawing_refactor.js
+++ drawing_refactor.js
@@ -19,6 +19,7 @@ var paddleNormalMatrix;
 // world matrices
 var baseWorldMatrix;
 var paddleWorldMatrix;
+var obstaclesWorldMatrix = new Array();
 
 // game status
 var stop = true;
@@ -32,8 +33,9 @@ var lightDirectionHandle = new Array(), lightColorHandle = new Array();
 var normalMatrixPositionHandle = new Array(), vertexMatrixPositionHandle = new Array();
 var baseColorLocation;
 var paddleColorLocation;
+var obstaclesColorLocation;
 
-// paddle coordinates
+// Paddle coordinates and status
 var originalPaddlePos = [0.0, 1.0, -4.5];
 var xPaddle = originalPaddlePos[0];
 var yPaddle = originalPaddlePos[1];
@@ -51,6 +53,10 @@ var xBall = originalBallPos[0];
 var yBall = originalBallPos[1];
 var zBall = originalBallPos[2];
 var ballCenter = originalBallPos;
+
+// Obstacles coordinates and status
+var obstaclesCenters = new Array();
+var obstacleHit = new Array();
 
 // persepctive and view
 var perspectiveMatrix;
@@ -72,22 +78,34 @@ var ballVertices;
 var ballNormals;
 var ballIndices;
 
-var obstaclesModels = new Array();
-var obstaclesVertices = new Array();
-var obstaclesNormals = new Array();
-var obstaclesIndices = new Array();
+var noObstacles = 5;
+var noRows = 2;
+var obstacleModel;
+var obstacleVertices;
+var obstacleNormals;
+var obstacleIndices;
 
 
 // colors
 var paddleColor;
 var baseColor;
+var ballColor;
+var obstacleColor1;
+var obstacleColor2;
 
 
 // vaos
 var paddleVAO;
 var baseVAO;
 var ballVAO;
-var obstaclesVAO;
+var obstacleVAO;
+
+
+
+// game status
+var lives = 3;
+var points = 0;
+var game_over = false;
 
 function main() {
 
@@ -102,10 +120,22 @@ function main() {
   paddleColor = [1.0, 0.0, 0.0];
   baseColor = [0.0, 0.5, 0.0];
   ballColor = [0.0, 0.0, 1.0];
+  obstacleColor1 = [0.5,0.5,0.5];
+  obstacleColor2 = [0.1,0.2,0.3];
+
 
   paddleWorldMatrix = utils.MakeWorld(xPaddle, yPaddle, zPaddle, 0.0, 0.0, 0.0, 1.0);
   baseWorldMatrix = utils.MakeWorld(0.0, 0.0, -15, 0.0, 0.0, 0.0, 1.0);
   ballWorldMatrix = utils.MakeWorld(xBall, yBall, zBall, 0.0, 0.0, 0.0, 1.0);
+
+  for (let i = 0; i < noRows; i++) {
+    for (let j = 0; j < noObstacles; j++) {
+      var [x, y, z] = [-4.3+myutils.round2dec(j*2.1),1.5,myutils.round2dec(-14.0-i*1.5)];
+      obstaclesCenters.push([x, y, z]);
+      obstacleHit.push(false);
+      obstaclesWorldMatrix.push(utils.MakeWorld(x, y, z, 0.0,0.0,0.0,1.0));
+    }
+  }
 
   //SET Global states (viewport size, viewport background color, Depth test)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -119,6 +149,7 @@ function main() {
   paddleColorLocation = gl.getUniformLocation(programs[2], "u_color");
   baseColorLocation = gl.getUniformLocation(programs[2], "u_color");
   ballColorLocation = gl.getUniformLocation(programs[2], "u_color");
+  obstaclesColorLocation = gl.getUniformLocation(programs[2], "u_color");
 
   // persepctive and view
   perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width/gl.canvas.height, 0.1, 100.0);
@@ -184,6 +215,25 @@ function main() {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(paddleIndices), gl.STATIC_DRAW);
 
+
+  // obstacles
+  obstacleVertices = obstacleModel.vertices;
+  obstacleNormals = obstacleModel.vertexNormals;
+  obstacleIndices = obstacleModel.indices;
+
+  obstacleVAO = gl.createVertexArray();
+
+  gl.bindVertexArray(obstacleVAO);
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obstacleVertices), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(positionAttributeLocation[2]);
+  gl.vertexAttribPointer(positionAttributeLocation[2], 3, gl.FLOAT, false, 0, 0);
+
+  var indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(obstacleIndices), gl.STATIC_DRAW);
+
   //---------------------------------------------------------------------------------------------
   drawScene();
 }
@@ -204,9 +254,7 @@ function animate(){
         direction = myutils.normalize3Vector(myutils.crossProduct(up, direction));
         dirDescription = [0,1,1,0];
       }
-      // console.log(dirDescription);
     } else if (xBall < limitLeft-0.5) { // collision with left wall
-      console.log(dirDescription);
       if (dirDescription[0] == 0 && dirDescription[1] == 0 && dirDescription[2] == 1 && dirDescription[3] == 0) { // perpendicular collision
         direction = [1.0,0.0,0.0];
         dirDescription = [0,0,0,1];
@@ -232,7 +280,7 @@ function animate(){
         direction = myutils.normalize3Vector(myutils.crossProduct(up, direction));
         dirDescription = [1,0,1,0];
       }
-    } else if (zBall >= zPaddle-1.0 && xBall >= xPaddle-1.0 && xBall <= xPaddle+1.0) { // collision with the paddle
+    } else if (zBall >= zPaddle-1.25 && xBall >= xPaddle-1.0 && xBall <= xPaddle+1.0) { // collision with the paddle
       if (xBall > xPaddle-0.1 && xBall < xPaddle+0.1) {
         direction = [0.0,0.0,-1.0];
         dirDescription = [1,0,0,0];
@@ -245,10 +293,71 @@ function animate(){
         }
         direction = myutils.normalize3Vector(centralDistance);
       }
+    } else{
+      for (let i = 0; i < noRows; i++) {
+        for (let j = 0; j < noObstacles; j++) {
+          if (!obstacleHit[j+noObstacles*i]) {
+            var [x, y, z] = obstaclesCenters[j+noObstacles*i];
+            if (zBall < z+0.25 && zBall > z+0.1 && xBall >= x-1.0 && xBall <= x+1.0) {  //  front bounce
+              if (xBall > x-0.5 && xBall < x+0.5) {
+                direction = [0.0,0.0,1.0];
+                dirDescription = [0,1,0,0];
+              } else {
+                var centralDistance = myutils.vectorDiff(ballCenter, [x, y, z]);
+                if (xBall >= x-1.0 && xBall < x-0.1) { // sx
+                  dirDescription = [0,1,1,0];
+                } else if (xBall <= x+1.0 && xBall > x+0.1) { // dx
+                  dirDescription = [0,1,0,1];
+                }
+                direction = myutils.normalize3Vector(centralDistance);
+              }
+              obstacleHit[j+noObstacles*i] = true;
+              points++;
+            } else if (zBall > z-2 && zBall < z-1.8 && xBall >= x-1.0 && xBall <= x+1.0) {  //  back bounce
+              console.log("back");
+              if (xBall > x-0.5 && xBall < x+0.5) {
+                direction = [0.0,0.0,-1.0];
+                dirDescription = [1,0,0,0];
+              } else {
+                var centralDistance = myutils.vectorDiff(ballCenter, [x, y, z]);
+                if (xBall >= x-1.0 && xBall < x-0.1) { // sx
+                  dirDescription = [1,0,1,0];
+                } else if (xBall <= x+1.0 && xBall > x+0.1) { // dx
+                  dirDescription = [1,0,0,1];
+                }
+                direction = myutils.normalize3Vector(centralDistance);
+              }
+              obstacleHit[j+noObstacles*i] = true;
+              points++;
+            } else if (xBall > x+0.2 && xBall < x+0.1 && zBall >= z-1.0 && zBall <= z+0.2) { // left bounce
+              var centralDistance = myutils.vectorDiff(ballCenter, [x, y, z]);
+              if (dirDescription[0] == 0 && dirDescription[0] == 1 && dirDescription[0] == 0 && dirDescription[0] == 1) {
+                dirDescription = [1,0,1,0];
+              } else {
+                dirDescription = [0,1,1,0];
+              }
+              direction = myutils.normalize3Vector(centralDistance);
+              obstacleHit[j+noObstacles*i] = true;
+              points++;
+            } else if (xBall < x-0.2 && xBall > x-0.1 && zBall >= z-1.0 && zBall <= z+0.2) { // right bounce
+              var centralDistance = myutils.vectorDiff(ballCenter, [x, y, z]);
+              if (dirDescription[0] == 0 && dirDescription[0] == 1 && dirDescription[0] == 1 && dirDescription[0] == 0) {
+                dirDescription = [1,0,0,1];
+              } else {
+                dirDescription = [0,1,0,1];
+              }
+              direction = myutils.normalize3Vector(centralDistance);
+              obstacleHit[j+noObstacles*i] = true;
+              points++;
+            }
+          }
+        }
+      }
     }
 
     if (zBall > zPaddle) {
       stop = true;
+      lives--;
       xPaddle = originalPaddlePos[0];
       xContr = originalPaddlePos[0];
       yPaddle = originalPaddlePos[1];
@@ -282,6 +391,8 @@ function animate(){
 
 function drawScene() {
   animate();
+
+  game_status();
 
   gl.enable(gl.DEPTH_TEST);
   gl.clearColor(0.85, 0.85, 0.85, 1.0);
@@ -321,8 +432,67 @@ function drawScene() {
   gl.drawElements(gl.TRIANGLES, ballIndices.length, gl.UNSIGNED_SHORT, 0 );
 
 
+  // obstacles
+  for (let i = 0; i < noRows; i++) {
+    for (let j = 0; j < noObstacles; j++) {
+      if (!obstacleHit[j+noObstacles*i]) {
+        gl.useProgram(programs[2]);
+        viewWorldMatrix = utils.multiplyMatrices(viewMatrix, obstaclesWorldMatrix[j+noObstacles*i]);
+        projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
+        gl.uniformMatrix4fv(matrixLocation[2], gl.FALSE, utils.transposeMatrix(projectionMatrix));
+        var obstacleColor = (i+j)%2==0 ? obstacleColor1 : obstacleColor2;
+        gl.uniform3fv(obstaclesColorLocation, obstacleColor);
+
+        gl.bindVertexArray(obstacleVAO);
+        gl.drawElements(gl.TRIANGLES, obstacleIndices.length, gl.UNSIGNED_SHORT, 0 );
+      }
+    }
+  }
+
   // drawScene
   window.requestAnimationFrame(drawScene);
+}
+
+function game_reset() {
+  stop = true;
+  xPaddle = originalPaddlePos[0];
+  xContr = originalPaddlePos[0];
+  yPaddle = originalPaddlePos[1];
+  zPaddle = originalPaddlePos[2];
+  paddleCenter = [xPaddle, yPaddle, zPaddle];
+
+  originalBallPos = [0, 0.5, zPaddle-1.5];
+  xBall = originalBallPos[0];
+  yBall = originalBallPos[1];
+  zBall = originalBallPos[2];
+  ballCenter = [xBall, yBall, zBall];
+
+  direction = [0.0,0.0,-1.0];
+  dirDescription = [1,0,0,0];
+
+  points = 0;
+  lives = 3;
+
+  for (let i = 0; i < noRows; i++) {
+    for (let j = 0; j < noObstacles; j++) {
+      obstacleHit[j+noObstacles*i] = false;
+    }
+  }
+
+  ballWorldMatrix = utils.MakeWorld(xBall, yBall, zBall, 0.0, 0.0, 0.0, 1.0);
+  paddleWorldMatrix = utils.MakeWorld(xPaddle, yPaddle, zPaddle, 0.0, 0.0, 0.0, 1.0);
+}
+
+function game_status() {
+  if (lives == 0) {
+    console.log("game over");
+    game_reset();
+  }
+  if (points == obstaclesCenters.length) {
+    console.log("win");
+    console.log(points);
+    game_reset();
+  }
 }
 
 async function init(){
@@ -372,6 +542,9 @@ async function init(){
   var ballStr = await utils.get_objstr(modelsDir+"sphere.obj");
   ballModel = new OBJ.Mesh(ballStr);
 
+  var obstacleStr = await utils.get_objstr(modelsDir+"obstacle.obj");
+  obstacleModel = new OBJ.Mesh(obstacleStr);
+
   main();
 }
 
@@ -379,12 +552,12 @@ window.addEventListener("keydown", function keyFunction(e){
 
   if (e.code == "ArrowLeft") {
     if (xContr > limitLeft) {
-      xContr-=0.1;
+      xContr-=0.25;
     }
   }
   if (e.code == "ArrowRight") {  // Right arrow
     if (xContr < limitRight) {
-      xContr+=0.1;
+      xContr+=0.25;
     }
   }
   if (e.code == "Space") {
